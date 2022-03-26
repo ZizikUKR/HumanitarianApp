248205c3bef162048d49cc451d75fbc8d0592aff
Artem Vasilenko <versuscolt@gmail.com>
2022-03-26 20:40:20 +0200

    Fix pop-up

--- HumanitarianApp.Api/client/src/components/pop-up/PopUp.js
+++ HumanitarianApp.Api/client/src/components/pop-up/PopUp.js
@@ -7,32 +7,13 @@ const PopUp = ({title, namePlaceholder, closePopUp, areaPlaceholder, selects}) =
     e.target.reset();
   };
 
-  const options = selects.map(select => {
-    if (select.id !== 0) {
-      return (
-        <option key={select.id - 1} value={select.id - 1}>{select.name}</option>
-      );
-    }
-  });
-
-  const selectList = () => {
-    if (selects.length !== 0) {
-      return (
-        <select className="pop-up__input pop-up__select" name="select" required>
-          <option value="">*Виберіть категорію</option>
-          {options}
-        </select>
-      )
-    }
-  };
-
   return (
     <div className="pop-up">
       <div className="pop-up__wrapper">
         <button className="pop-up__close" onClick={closePopUp}>&times;</button>
         <h2 className="subtitle">{title}</h2>
         <form className="pop-up__form" action="#" onSubmit={e => onSubmit(e)}>
-          {selectList()}
+          <Categories selects={selects} />
           <input className="pop-up__input" 
             name="name"
             type="text"
@@ -69,6 +50,36 @@ const PopUp = ({title, namePlaceholder, closePopUp, areaPlaceholder, selects}) =
       <div className="pop-up__overlay" onClick={closePopUp}></div>
     </div>
   );
-}
+};
+
+const Categories = ({selects}) => {
+  const options = selects.map(select => {
+    if (select.id !== 0) {
+      return (
+        <option key={select.id - 1} value={select.id - 1}>{select.name}</option>
+      );
+    }
+  });
+
+  if (selects.length !== 0) {
+    return (
+      <select className="pop-up__input pop-up__select" name="categories" required>
+        <option value="">*Виберіть категорію</option>
+        {options}
+      </select>
+    )
+  }
+
+  return null;
+};
+
+const BankAccount = () => {
+  return (
+    <select className="pop-up__input pop-up__select" name="bank">
+      <option value="0">Номер банківської картки</option>
+      <option value="1">Номер банківського рахунку</option>
+    </select>
+  );
+};
 
 export default PopUp;
