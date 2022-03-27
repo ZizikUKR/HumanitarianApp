2fdcc4f2854f07bee4d2a89205bbbfa70091cc2f
Mykola Zinenko <mzine@softserveinc.com>
2022-03-27 13:58:22 +0300

    added file logging and middleware exception

--- HumanitarianApp.Api/Program.cs
+++ HumanitarianApp.Api/Program.cs
@@ -1,10 +1,18 @@
+using HumanitarianApp.Api.Middlewares;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+var logger = new LoggerConfiguration()
+  .ReadFrom.Configuration(builder.Configuration)
+  .Enrich.FromLogContext()
+  .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,7 +22,6 @@ builder.Services.AddDbContext<HumanitarianApp.DAL.HumanityDb.HumanitarianDbConte
         b => b.MigrationsAssembly("HumanitarianApp.Api")));
 
 var app = builder.Build();
-
 using (var scope = app.Services.CreateScope())
 {
     var dataContext = scope.ServiceProvider.GetRequiredService<HumanitarianApp.DAL.HumanityDb.HumanitarianDbContext>();
@@ -27,6 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseHttpStatusCodeExceptionMiddleware();
 
 app.UseHttpsRedirection();
 
