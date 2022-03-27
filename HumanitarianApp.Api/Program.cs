667814d29f0ab09289740418311a5ae66d9eff6b
Mykola Zinenko <mzine@softserveinc.com>
2022-03-27 14:15:51 +0300

    added cors policy

--- HumanitarianApp.Api/Program.cs
+++ HumanitarianApp.Api/Program.cs
@@ -3,11 +3,26 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// ConfigureCors
+{
+    IConfigurationSection corsOptions = builder.Configuration.GetSection("Cors");
+    string origins = corsOptions["Origins"];
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("OriginPolicy", builder =>
+        {
+            builder.WithOrigins(origins.Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray())
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+    });
+}
 
 builder.Services.AddDbContext<HumanitarianApp.DAL.HumanityDb.HumanitarianDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("HumanitarianConnection"), 
@@ -27,6 +42,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("OriginPolicy");
 
 app.UseHttpsRedirection();
 
