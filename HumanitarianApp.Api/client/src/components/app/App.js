ea57fb898f58e9c898f6d624960ad6a1302e76e3
Mykola Zinenko <mzine@softserveinc.com>
2022-04-07 21:48:12 +0300

    removed unused code

--- HumanitarianApp.Api/client/src/components/app/App.js
+++ HumanitarianApp.Api/client/src/components/app/App.js
@@ -38,86 +38,6 @@ class App extends Component {
             {id: 4, name: 'Інше'}
           ],
           ads: [
-            {
-              "select": "Перевезення",
-              "name": "Петров Іван Сергійович",
-              "telephone": "+38099112233",
-              "email": "petrov@qwerty.com",
-              "city": "Київ",
-              "address": "м. Перемоги, 1",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas.",
-              "cardnumber": "12345678901234",
-              "fullbankname": "GgggMmmmBbbbHhhh Bank",
-              "shortbankname": "GMBH Bank",
-              "mfo": "8465",
-              "iban": "2546542544",
-              "edrpou": "12345678",
-              "accountnumber": "484654654465454845"
-            },
-            {
-              "select": "Гуманітарна допомога",
-              "name": "Олександров Василь Петрович",
-              "telephone": "+380669998877",
-              "email": "oleksandrov@qwerty.com",
-              "city": "Днепр",
-              "address": "пр. Свободи, 47/12",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam architecto numquam aliquam quas aspernatur voluptatibus harum non cumque excepturi? Nesciunt reiciendis, autem deleniti debitis asperiores nulla ipsa sapiente doloribus officiis.",
-              "cardnumber": "",
-              "fullbankname": "",
-              "shortbankname": "",
-              "mfo": "",
-              "iban": "",
-              "edrpou": "",
-              "accountnumber": ""
-            },
-            {
-              "select": "Інше",
-              "name": "Фомін Віталій Володимирович",
-              "telephone": "+380674445566",
-              "email": "fomin@qwerty.com",
-              "city": "Запоріжжя",
-              "address": "",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam architecto numquam aliquam quas aspernatur voluptatibus harum non cumque excepturi? Nesciunt reiciendis, autem deleniti debitis asperiores nulla ipsa sapiente doloribus officiisa nulla ipsa sapiente doloribus officiis qwertyuiopas.",
-              "cardnumber": "",
-              "fullbankname": "",
-              "shortbankname": "",
-              "mfo": "",
-              "iban": "",
-              "edrpou": "",
-              "accountnumber": ""
-            },
-            {
-              "select": "Гуманітарна допомога",
-              "name": "Петров Александр Васильович",
-              "telephone": "+380687778899",
-              "email": "",
-              "city": "Днепр",
-              "address": "",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam architecto numquam aliquam quas aspernatur voluptatibus harum non cumque excepturi? Nesciunt reiciendis, autem deleniti debitis asperiores nulla ipsa sapiente doloribus officiis.",
-              "cardnumber": "43219876543210",
-              "fullbankname": "",
-              "shortbankname": "",
-              "mfo": "8465",
-              "iban": "2546542544",
-              "edrpou": "0123456789",
-              "accountnumber": "484654654465454845"
-            },
-            {
-              "select": "Медицина",
-              "name": "Дубровський Георгій В'ячеславович",
-              "telephone": "+380633211221",
-              "email": "heorhii@qwerty.com",
-              "city": "Хмельницький",
-              "address": "мм. Квітневий, 27/80",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam architecto numquam aliquam quas aspernatur voluptatibus harum non cumque excepturi? Nesciunt reiciendis, autem deleniti debitis asperiores nulla ipsa sapiente doloribus officiisa nulla ipsa sapiente doloribus officiis qwertyuiopas.",
-              "cardnumber": "12345678",
-              "fullbankname": "йфяцічувскамепинртгоьшлбщд",
-              "shortbankname": "йфяцічусскам",
-              "mfo": "123456566654546",
-              "iban": "64654654465465465465",
-              "edrpou": "0123456789",
-              "accountnumber": "апу65+5+65ппукпук5п565пку546"
-            }
           ]
         },
         {
@@ -127,14 +47,6 @@ class App extends Component {
           filter: 'Усі категорії',
           filterButtons: [],
           ads: [
-            {
-              "name": "Фірма",
-              "telephone": "+380674445566",
-              "email": "firma@qwerty.com",
-              "city": "Львів",
-              "address": "",
-              "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam sit ipsum neque. Tempore totam velit veniam eius a deleniti excepturi tenetur unde alias sunt quia aliquid, placeat aperiam odio voluptas. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam architecto numquam aliquam quas aspernatur voluptatibus harum non cumque excepturi?."
-            }
           ]
         },
         {
@@ -228,13 +140,11 @@ class App extends Component {
   service = new Service();
   componentDidMount() {
     this.getAllVolunteers();
-    this.getAllOrganizations();
-    this.getAllAnnouncements();
   }
 
   getAllVolunteers = () => {
     this.service
-      .get("https://localhost:7057/api/Volunteer/GetAll?pageNumber=1")
+      .get("https://localhost:7057/api/Volunteer/GetAllActive?pageNumber=1")
       .then((response) => {
         const volonteers = response.map((item) => {
           return {
@@ -264,54 +174,6 @@ class App extends Component {
       
   };
 
-  getAllOrganizations = () => {
-    this.service
-      .get("https://localhost:7057/api/Organization/GetAll?pageNumber=1")
-      .then((response) => {
-        const organizations = response.map((item) => {
-          return {
-            name: item.name,
-            telephone: item.phoneNumber,
-            email: item.email,
-            city: item.city,
-            address: item.address,
-            text: item.description,
-          };
-        });
-
-        let newsectionsData = Object.assign({}, this.state.sectionsData);
-        newsectionsData[1].ads = organizations;
-
-        this.setState({ sectionsData: newsectionsData });
-      });
-
-      
-  };
-
-  getAllAnnouncements = () => {
-    this.service
-      .get("https://localhost:7057/api/Announcement/GetAll?pageNumber=1")
-      .then((response) => {
-        const announcements = response.map((item) => {
-          return {
-            select: this.setAnnouncementCategory(item.category),
-            name: item.name,
-            telephone: item.phoneNumber,
-            email: item.email,
-            city: item.city,
-            address: item.address,
-            text: item.description,
-          };
-        });
-        
-
-        let newsectionsData = Object.assign({}, this.state.sectionsData);
-        newsectionsData[2].ads = announcements;
-
-        this.setState({ sectionsData: newsectionsData });
-      });
-  };
-
   setVolunteersCategory = (category) => {
     if (category == 1) {
       return "Перевезення";
@@ -332,37 +194,6 @@ class App extends Component {
     return "";
   };
 
-  setAnnouncementCategory = (category) => {
-    if (category == 0) {
-      return "Шукаю";
-    }
-
-    if (category == 1) {
-      return "Знайшов";
-    }
-
-    if (category == 2) {
-      return "Продам";
-    }
-
-    if (category == 3) {
-      return "Придбаю";
-    }
-
-    if (category == 4) {
-      return "Вiддам";
-    }
-
-    if (category == 5) {
-      return "Прийму";
-    }
-
-    if (category == 6) {
-      return "Iнше";
-    }
-
-    return "";
-  };
 
   render() {
     const {headerButtons, visibleBurgerMenu, sectionsData, activeSection, openAgreement} = this.state;
