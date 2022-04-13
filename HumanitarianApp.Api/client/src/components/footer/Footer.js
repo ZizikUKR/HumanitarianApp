811b5e197355343090ad402ffc12e84b9915d7d0
Yevhen <jekson949@gmail.com>
2022-04-13 15:28:54 +0300

    change footer and added icon email

--- HumanitarianApp.Api/client/src/components/footer/Footer.js
+++ HumanitarianApp.Api/client/src/components/footer/Footer.js
@@ -6,15 +6,14 @@ const Footer = () => {
       <div className="container">
         <div className="footer__wrapper">
           <div className="footer__copyright">2022 &copy; humanitarian.org</div>
-          <div className="footer__contacts">
-            Наш email: <a href="mailto:qwerty@gmail.com" className="footer__email">qwerty@gmail.com</a>
-          </div>
-        </div>
+        </div>        
+        <div className="footer__copyright">Наши контакты:</div>
         <ul class="social-icons" >
           <li><a class="social-icon-twitter" href="#" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a class="social-icon-fb" href="https://www.facebook.com/profile.php?id=100002714508841" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a class="social-icon-fb" href="https://www.facebook.com/kolomiiitsev/" title="..." target="_blank" rel="noopener"></a></li>
           <li><a class="social-icon-telegram" href="https://t.me/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
           <li><a class="social-icon-instagram" href="https://www.instagram.com/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a class="social-icon-email" href="mailto:qwerty@gmail.com" title="..." target="_blank" rel="noopener"></a></li>
         </ul>      
       </div>      
     </footer>
