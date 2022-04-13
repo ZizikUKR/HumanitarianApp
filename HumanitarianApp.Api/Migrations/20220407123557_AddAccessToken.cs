64749f95fbc50d601b64c1e90d3389007a946515
Yrii2599 <51967238+Yrii2599@users.noreply.github.com>
2022-04-13 13:03:51 +0300

    Add Access token and Refresh token (#11)
    * Add Access token and Refresh token
    
    * Add refresh token from front end side

---
+++ HumanitarianApp.Api/Migrations/20220407123557_AddAccessToken.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HumanitarianApp.Api.Migrations
+{
+    public partial class AddAccessToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "90271977-1764-457c-b6fa-d2afc9f5b899");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "78d5b2b5-50a6-4fdd-9f13-44c67e4ab01f", "c7951e46-b6bc-4aa7-94dc-e8e1094fbbc4", "Administrator", "ADMINISTRATOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "78d5b2b5-50a6-4fdd-9f13-44c67e4ab01f");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "90271977-1764-457c-b6fa-d2afc9f5b899", "960f4012-78c1-450e-9100-6929cfbb1c4f", "Administrator", "ADMINISTRATOR" });
+        }
+    }
+}
