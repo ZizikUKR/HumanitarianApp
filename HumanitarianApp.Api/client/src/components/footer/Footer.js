6b00b7dc2f50d77825a6b5ce7a078065c3920272
Yevhen <jekson949@gmail.com>
2022-04-15 12:47:42 +0300

    deleted unused css classes and changed REM on PX

--- HumanitarianApp.Api/client/src/components/footer/Footer.js
+++ HumanitarianApp.Api/client/src/components/footer/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
         <div className="footer__wrapper">
           <div className="footer__copyright">2022 &copy; humanitarian.org</div>
         </div>
-        <div className="footer__copyright">Наші контакти:</div>
-        <ul class="social-icons" >
-          <li><a class="social-icon-twitter" href="#" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a class="social-icon-fb" href="https://www.facebook.com/kolomiiitsev/" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a class="social-icon-telegram" href="https://t.me/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a class="social-icon-instagram" href="https://www.instagram.com/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a class="social-icon-email" href="mailto:qwerty@gmail.com" title="..." target="_blank" rel="noopener"></a></li>
+        <div className="footer__contaks">Наші контакти:</div>
+        <ul className="social-icons" >
+          <li><a className="social-icon-twitter" href="#" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a className="social-icon-fb" href="https://www.facebook.com/kolomiiitsev/" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a className="social-icon-telegram" href="https://t.me/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a className="social-icon-instagram" href="https://www.instagram.com/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
+          <li><a className="social-icon-email" href="mailto:qwerty@gmail.com" title="..." target="_blank" rel="noopener"></a></li>
         </ul>      
       </div>      
     </footer>
