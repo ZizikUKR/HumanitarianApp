74e332fcb761fd1e0533211b7436b4a43d7c4d56
VasiletsFour <vasilets.seven@gmail.com>
2022-04-23 23:24:44 +0300

    refactor index.js file (move layer in app.tsx)

--- HumanitarianApp.Api/client/src/index.js
+++ HumanitarianApp.Api/client/src/index.js
@@ -1,18 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Normalize } from 'styled-normalize';
-import { BrowserRouter } from 'react-router-dom';
-
-import App from './components/app/App';
-
+import App from './App';
 import './styles/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Normalize />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
+root.render(<App/>);
