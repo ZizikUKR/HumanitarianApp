9af1fa80aeed025ba813c13efbccb7e6cf9e04ce
VasiletsFour <vasilets.seven@gmail.com>
2022-04-23 23:29:57 +0300

    create n-layer architecture route->page->container

---
+++ HumanitarianApp.Api/client/src/container/VolunteerContainer.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Main from "../components/main/Main"
+
+export const VolunteerContainer = () => {
+    return (<Main/>)
+}
---
+++ HumanitarianApp.Api/client/src/pages/VolunteerPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {PageLayout} from "../layouts/pageLayout/PageLayout";
+import {VolunteerContainer} from "../container/VolunteerContainer";
+
+export const VolunteerPage = () => (
+    <PageLayout>
+        <VolunteerContainer/>
+    </PageLayout>
+)
