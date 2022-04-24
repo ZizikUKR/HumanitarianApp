4ab00ff12468a2c88a4530339553ea94f911cb25
VasiletsFour <vasilets.seven@gmail.com>
2022-04-24 15:21:07 +0300

    Refactor PublicNavigation top visible

--- HumanitarianApp.Api/client/src/navigation/PublicNavigation.tsx
+++ HumanitarianApp.Api/client/src/navigation/PublicNavigation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {VolunteerPage} from "../pages/VolunteerPage";
+import AdminLogin from "../components/admin-login/AdminLogin";
+import {ADMIN_LOGIN_URL, PUBLIC_URL} from "../modules/url/publicUrl.const";
 
-export const PublicNavigation = () => (
-    <Routes>
-        <Route path="/" element={<VolunteerPage/>}/>
-    </Routes>
-)
+export const PublicNavigation = [
+    <Route index key={PUBLIC_URL} element={<VolunteerPage/>}/>,
+    <Route path={ADMIN_LOGIN_URL} key={ADMIN_LOGIN_URL} element={<AdminLogin/>}/>
+]
+
 
