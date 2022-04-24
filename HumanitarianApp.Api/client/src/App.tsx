bb3e6798244b669eedb6c9098b8fa768ee70d061
VasiletsFour <vasilets.seven@gmail.com>
2022-04-24 15:20:01 +0300

    Refactor app routes top view

--- HumanitarianApp.Api/client/src/App.tsx
+++ HumanitarianApp.Api/client/src/App.tsx
@@ -1,26 +1,15 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import React from 'react';
 import AuthProvider from "./services/AuthProvider"
-import {PublicNavigation} from "./navigation/PublicNavigation";
-import {AdminRoute} from "./routes/AdminRoute";
-import Page404 from "./components/page-404/Page404";
 import './styles/app.scss';
-import {PUBLIC_URL} from "./modules/url/publicUrl.const";
-import {ADMIN_URL} from "./modules/url/adminUrl.const";
+import "./styles/app.scss";
+import {AppNavigation} from "./navigation/Navigation";
 
 const App = () => (
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
-                <Routes>
-                    <Route path={PUBLIC_URL}>
-                        {PublicNavigation}
-                    </Route>
-                    <Route path={ADMIN_URL} element={<AdminRoute/>}>
-                        <Route index/>
-                    </Route>
-                    <Route path="*" element={<Page404/>}/>
-                </Routes>
+                <AppNavigation/>
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
