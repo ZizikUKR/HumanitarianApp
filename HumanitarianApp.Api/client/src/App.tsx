da9118a13f79cf75c604b3e214c5f17eb257c753
VasiletsFour <vasilets.seven@gmail.com>
2022-04-24 15:16:33 +0300

    Refactor app routes top view

--- HumanitarianApp.Api/client/src/App.tsx
+++ HumanitarianApp.Api/client/src/App.tsx
@@ -1,38 +1,29 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from 'react';
 import AuthProvider from "./services/AuthProvider"
-
-import './styles/app.scss';
 import {PublicNavigation} from "./navigation/PublicNavigation";
 import {AdminRoute} from "./routes/AdminRoute";
+import Page404 from "./components/page-404/Page404";
+import './styles/app.scss';
+import {PUBLIC_URL} from "./modules/url/publicUrl.const";
+import {ADMIN_URL} from "./modules/url/adminUrl.const";
 
 const App = () => (
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
-                    <Route path={"*"} element={<PublicNavigation/>}/>
-                    <Route path={"/adin"} element={<AdminRoute/>}>
+                    <Route path={PUBLIC_URL}>
+                        {PublicNavigation}
+                    </Route>
+                    <Route path={ADMIN_URL} element={<AdminRoute/>}>
                         <Route index/>
                     </Route>
-                    {/*<Route path={"/test"} element={<Main/>}/>*/}
-                    {/*    /!*<Route path="/" element={<PageLayout/>}>*!/*/}
-                    {/*    <Route path={"/"} element={<VolunteerPage/>}/>*/}
-                    {/*    /!*<PublicNavigation/>*!/*/}
-
-                    {/*    /!*</Route>*!/*/}
-                    {/*    /!*<Route path="/" element={<Main/>}/>*!/*/}
-                    {/*    <Route path="/admin-login" element={<AdminLogin/>}/>*/}
-                    {/*    <Route path="/admin-panel" element={// <RequireAuth>*/}
-                    {/*        //   <AdminPanel />*/}
-                    {/*        // </RequireAuth>*/}
-                    {/*        <AdminPanel/>}/>*/}
-                    {/*    <Route path="*" element={<Page404/>}/>*/}
+                    <Route path="*" element={<Page404/>}/>
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
-
 
 export default App;
--- HumanitarianApp.Api/client/src/components/footer/Footer.jsx
+++ HumanitarianApp.Api/client/src/components/footer/Footer.jsx
@@ -1,23 +1,24 @@
-import './footer.scss';
+import React from "react";
+// import './footer.scss';
 
-const Footer = () => {
-  return(
+export const Footer = () => (
     <footer className="footer">
-      <div className="container">
-        <div className="footer__wrapper">
-          <div className="footer__copyright">2022 &copy; humanitarian.org</div>
+        <div className="container">
+            <div className="footer__wrapper">
+                <div className="footer__copyright">2022 &copy; humanitarian.org</div>
+            </div>
+            <div className="footer__copyright">Наші контакти:</div>
+            <ul className="social-icons">
+                <li><a className="social-icon-twitter" href="#" title="..." target="_blank" rel="noopener"/></li>
+                <li><a className="social-icon-fb" href="https://www.facebook.com/kolomiiitsev/" title="..."
+                       target="_blank" rel="noopener"/></li>
+                <li><a className="social-icon-telegram" href="https://t.me/kolomiiitsev" title="..." target="_blank"
+                       rel="noopener"/></li>
+                <li><a className="social-icon-instagram" href="https://www.instagram.com/kolomiiitsev" title="..."
+                       target="_blank" rel="noopener"/></li>
+                <li><a className="social-icon-email" href="mailto:qwerty@gmail.com" title="..." target="_blank"
+                       rel="noopener"/></li>
+            </ul>
         </div>
-        <div className="footer__copyright">Наші контакти:</div>
-        <ul className="social-icons" >
-          <li><a className="social-icon-twitter" href="#" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a className="social-icon-fb" href="https://www.facebook.com/kolomiiitsev/" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a className="social-icon-telegram" href="https://t.me/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a className="social-icon-instagram" href="https://www.instagram.com/kolomiiitsev" title="..." target="_blank" rel="noopener"></a></li>
-          <li><a className="social-icon-email" href="mailto:qwerty@gmail.com" title="..." target="_blank" rel="noopener"></a></li>
-        </ul>      
-      </div>      
     </footer>
-  );
-}
-
-export default Footer;
+);
