b506fed6934fad0b9a64889b0c92242ff6bca567
VasiletsFour <vasilets.seven@gmail.com>
2022-04-26 00:50:52 +0300

    delete state const from public containers

--- HumanitarianApp.Api/client/src/container/EnterprisesConteiner.tsx
+++ HumanitarianApp.Api/client/src/container/EnterprisesConteiner.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import AdList from "components/ad-list/AdList";
 import {sectionMoc} from "shared/modules/layout/layoutData.moc";
 
-export const EnterprisesContainer = () => {
-    const [section] = React.useState(sectionMoc["Підприємства"])
+const section = sectionMoc["Підприємства"]
 
+export const EnterprisesContainer = () => {
     return (<AdList id={section.id} ads={section.ads} numberAdsOfPages={0}/>)
 }
--- HumanitarianApp.Api/client/src/container/VolunteerContainer.tsx
+++ HumanitarianApp.Api/client/src/container/VolunteerContainer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import AdList from "components/ad-list/AdList";
 import {sectionMoc} from "shared/modules/layout/layoutData.moc";
 
-export const VolunteerContainer = () => {
-    const [section] = React.useState(sectionMoc["Волонтери"])
+const section = sectionMoc["Волонтери"];
 
+export const VolunteerContainer = () => {
     return (<AdList id={section.id} ads={section.ads} numberAdsOfPages={0}/>)
 }
