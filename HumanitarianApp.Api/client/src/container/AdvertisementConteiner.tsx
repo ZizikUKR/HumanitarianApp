f4ec7afbc240cbe4d99d3b2f0b579c1d46c1165d
VasiletsFour <vasilets.seven@gmail.com>
2022-04-26 14:32:12 +0300

    fix section name is AdvertisementConteiner

--- HumanitarianApp.Api/client/src/container/AdvertisementConteiner.tsx
+++ HumanitarianApp.Api/client/src/container/AdvertisementConteiner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import AdList from "components/ad-list/AdList";
 import {sectionMoc} from "shared/modules/layout/layoutData.moc";
 
-const section = sectionMoc["Підприємства"];
+const section = sectionMoc["Оголошення"];
 
 export const AdvertisementContainer = () => {
     return (<AdList id={section.id} ads={section.ads} numberAdsOfPages={0}/>)
