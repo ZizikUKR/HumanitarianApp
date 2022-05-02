fe72fb6ad6d3eb54c4ff211c4351b3b89c9270ee
Yevhen <jekson949@gmail.com>
2022-05-02 11:58:32 +0300

    created page informations lovintir

--- HumanitarianApp.Api/client/src/components/card/Card.jsx
+++ HumanitarianApp.Api/client/src/components/card/Card.jsx
@@ -1,45 +1,46 @@
-import { Component } from "react";
 import Service from "../../services/Service";
-import {isEmpty} from "./isEmpty.jsx";
-import {setVolunteersCategory} from "./setVolunteersCategory.jsx";
-import AdList from "../ad-list/AdList";
+import { useParams } from 'react-router-dom';
+import { isEmpty } from './isEmpty.jsx'
+import { setVolunteersCategory } from './setVolunteersCategory.jsx'
+import { removeHttpFromUrl } from "./removeHttpFromUrl";
 
 import './card.scss';
+import { useState, useEffect } from "react";
+import Footer from "../footer/Footer";
 
 
-export default class Card extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      volunteerPostObj: {
-        cardId: "",
-        name: "",
-        phoneNumber: "",
-        email: "",
-        city: "",
-        address: "",
-        description: "",
-        category: "",
-        telegram: "",
-        instagram: "",
-        facebook: "",
-        bankDetails: {
-          fullBankName: "",
-          shortBankName: "",
-          iban: "",
-          accountNumber: "",
-          mfo: "",
-          edrpo: "",
-          cardNumber: "",
-        },
-      },
-    };
-  } 
+const Card = () => {
 
-  getVolunteerById = (cardId) => {
-    this.service
+  const [volunteerPostObj, setVolunteerPostObj ] = useState ({
+    cardId: "",
+    name: "",
+    phoneNumber: "",
+    email: "",
+    city: "",
+    address: "",
+    description: "",
+    category: "",
+    telegram: "",
+    instagram: "",
+    facebook: "",
+    bankDetails: {
+      fullBankName: "",
+      shortBankName: "",
+      iban: "",
+      accountNumber: "",
+      mfo: "",
+      edrpo: "",
+      cardNumber: "",
+    }
+  });
+
+  const { id } = useParams();
+
+  const  getVolunteerById = () => {
+    const service = new Service();
+    service
       .get(
-        `${process.env.REACT_APP_API_URL}Volunteer/GetById?id=${cardId}`
+        `${process.env.REACT_APP_API_URL}Volunteer/GetById?id=${id}`
       )
       .then((item) => {
         const volonteer = {
@@ -62,41 +63,77 @@ export default class Card extends Component {
           edrpou: isEmpty(item.bankDetails?.edrpo),
           accountnumber: isEmpty(item.bankDetails?.accountNumber),
         };
-        this.setState({ volunteerPostObj: volonteer });
+        setVolunteerPostObj(volonteer);
       });
   };
 
-  service = new Service();
-  componentDidMount() {
-    this.getVolunteerById();
-  }
+  useEffect (() => {
+      getVolunteerById();
+    }, []
+  );
 
-  render() {
-    return (
-      <>
-        <div className="card__item">
-          <div className="container">
-            <div className="card__name">{this.state.volunteerPostObj.name} - {this.state.volunteerPostObj.category}</div>
-            <div className="card__information">
-              <div className="card__text">{this.state.volunteerPostObj.text}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.telephone}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.email}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.city}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.address}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.instagram}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.telegram}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.facebook}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.cardnumber}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.fullbankname}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.shortbankname}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.mfo}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.iban}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.edrpo}</div>
-              <div className="card__contacts">{this.state.volunteerPostObj.accountNumber}</div>
+  return (
+    <>
+      <div className="card__item">
+        <div className="container">
+          <div className="card__name">
+            <p>{volunteerPostObj.name}</p>
+            <p>{volunteerPostObj.select}</p>
+          </div>
+          <ul className='ad-list__share'>
+            <li>
+              <a className="ad-list__share-link ad-list__share-link--fb" href="#">
+                <span>Поділитися:</span>
+              </a>
+            </li>
+            <li>
+              <a className="ad-list__share-link ad-list__share-link--telegram" href="#">
+                <span>Поділитися:</span>
+              </a>
+            </li>
+            <li>
+              <a className="ad-list__share-link ad-list__share-link--instagram" href="#">
+                <span>Поділитися:</span>
+              </a>
+            </li>
+          </ul>
+          
+          <div className="card__information">
+            <div className="card__image">
+              <img className="card__image-style" src="https://static.ukrinform.com/photos/2017_10/1507935996-3775.jpg"/>
+              <p className="card__text">{volunteerPostObj.text}</p>
+            </div>
+            <div>
+              <div className="card__information-volontir">
+                <div className="card__information-mini">
+                  <p className="card__information-title">Особиста інформація</p>
+                    <p className="card__contacts"><b>Телефон:</b> {volunteerPostObj.telephone}</p>
+                    <p className="card__contacts"><b>Email:</b> {volunteerPostObj.email}</p>
+                    <p className="card__contacts"><b>Місто:</b> {volunteerPostObj.city}</p>
+                    <p className="card__contacts"><b>Адресса:</b> {volunteerPostObj.address}</p>
+                    <p className="card__contacts"><b>Instagram: </b><a href={volunteerPostObj.instagram} target="_blank">{removeHttpFromUrl(volunteerPostObj.instagram)}</a></p>
+                    <p className="card__contacts"><b>Telegram: </b><a href={volunteerPostObj.telegram} target="_blank">{removeHttpFromUrl(volunteerPostObj.telegram)}</a></p>
+                    <p className="card__contacts"><b>Facebook: </b><a href={volunteerPostObj.facebook} target="_blank">{removeHttpFromUrl(volunteerPostObj.facebook)}</a></p>
+                </div>
+                <div className="card__information-mini">
+                  <p  className="card__information-title">Інформація банку</p>
+                    <p className="card__contacts"><b>Номер картки:</b> {volunteerPostObj.cardnumber}</p>
+                    <p className="card__contacts"><b>Повна назва банку:</b> {volunteerPostObj.fullbankname}</p>
+                    <p className="card__contacts"><b>Скорочена назва банку:</b> {volunteerPostObj.shortbankname}</p>
+                    <p className="card__contacts"><b>MFO:</b> {volunteerPostObj.mfo}</p>
+                    <p className="card__contacts"><b>IBAN:</b> {volunteerPostObj.iban}</p>
+                    <p className="card__contacts"><b>EDRPO:</b> {volunteerPostObj.edrpo}</p>
+                    {this.volunteerPostObj.accountnumber.length > 0 ?<p className="card__contacts"><b>PP:</b> {volunteerPostObj.accountNumber}</p> : null }
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </>
-    );
+      </div>
+    <Footer />
+    </>
+    
+  );
   }
-}
+
+  export default Card;
